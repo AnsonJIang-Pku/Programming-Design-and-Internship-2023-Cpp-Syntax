3897e47c490a0ef2a584666eb7821258740cfe7f
AnsonJIang-Pku <75867171+AnsonJIang-Pku@users.noreply.github.com>
2023-03-15 15:22:19 +0800

    Create B:看上去好坑的运算符重载.cpp

---
+++ 2023 程序设计实习之运算符重载作业/B:看上去好坑的运算符重载.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+class MyInt 
+{ 
+	int nVal; 
+	public: 
+	MyInt( int n) { nVal = n ;}
+    operator int() { return nVal; }
+    MyInt& operator-(int n)
+    {
+        nVal -= n;
+        return *this;
+    }
+}; 
+int Inc(int n) {
+	return n + 1;
+}
+int main () { 
+	int n;
+	while(cin >>n) {
+		MyInt objInt(n); 
+		objInt-2-1-3; 
+		cout << Inc(objInt);
+		cout <<","; 
+		objInt-2-1; 
+		cout << Inc(objInt) << endl;
+	}
+	return 0;
+}
