c7be6cd914daae709f924b766006b3f76fc973fd
AnsonJIang-Pku <75867171+AnsonJIang-Pku@users.noreply.github.com>
2023-03-15 13:41:35 +0800

    Create C:惊呆！Point竟然能这样输入输出.cpp
    输入输出流运算符的重载，友元

---
+++ 2023 程序设计实习之运算符重载作业/C:惊呆！Point竟然能这样输入输出.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+
+#include <iostream>
+using namespace std;
+class Point {
+    private:
+        int x;
+        int y;
+    public:
+        Point() { };
+    
+    //可以通过friend在class内实现iostream重载
+    friend istream& operator>>(istream & is,Point& p)
+    {
+        is >> p.x >> p.y;
+        return is;
+    }
+    friend ostream& operator << (ostream & os, const Point& p)
+    {
+        os << p.x << "," << p.y;
+        return os;
+    }
+
+};
+int main()
+{
+     Point p;
+     while(cin >> p) {
+         cout << p << endl;
+     }
+    return 0;
+}
