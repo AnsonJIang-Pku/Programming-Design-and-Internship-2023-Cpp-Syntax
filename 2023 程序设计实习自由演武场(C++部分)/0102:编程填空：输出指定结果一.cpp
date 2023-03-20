6048f7da621a31b2b92b5d974d02b14a2993d99d
AnsonJIang-Pku <75867171+AnsonJIang-Pku@users.noreply.github.com>
2023-03-20 16:34:16 +0800

    Create 0102:编程填空：输出指定结果一.cpp

---
+++ 2023 程序设计实习自由演武场(C++部分)/0102:编程填空：输出指定结果一.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Number {
+public:
+    int num;
+    Number(int n): num(n) {
+    }
+int & value()
+    {
+        return num;
+    }
+    Number operator+(const Number& b)
+    {
+        num = num+b.num;
+        return *this;
+    }
+};
+int main() {
+    Number a(2);
+    Number b = a;
+    cout << a.value() << endl;
+    cout << b.value() << endl;
+    a.value() = 8;
+    cout << a.value() << endl;
+    a+b;
+    cout << a.value() << endl;
+    return 0;
+}
